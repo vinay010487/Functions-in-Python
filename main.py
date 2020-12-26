60476f64e90cdbea8fefa0642b1d2639021b77ba
VinayRVishwakarma <36599592+vinay010487@users.noreply.github.com>
2020-12-26 14:49:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+def genre():
+  x='pop'
+  return x
+
+def artist():
+  y='Marshmello and Anne-Marie'
+  return y
+
+def Language():
+  x='English'
+  return x
+
+def Number_of_artist():
+  x=2
+  return x
+
+def artist_is_british():
+  print(bool('Yes'))
+
+
+artist_is_british()
